744656403c6cc8eb7b7aa38cacc6218737d60972
pahnupong660610783 <pahnupong_kaeopramun@cmu.ac.th>
2025-03-22 22:52:53 +0700

    เหมือนกัน แต่มะกี้ลืมใส่

--- frontend/kom_bat/src/components/Home/Start.js
+++ frontend/kom_bat/src/components/Home/Start.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import B1 from './ปุ่ม/B1.png';
 import './Start.css';
-const Start = () => ( <h1> 
-  <img className="hover-image" src={B1} alt="Hover Image" width="200" />
-</h1>
+
+const Start = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/gamemode');
+  };
+
+  return (
+    <h1> 
+      <img 
+        className="hover-image" 
+        src={B1} 
+        alt="Hover Image" 
+        width="200" 
+        onClick={handleClick}
+        style={{cursor: 'pointer'}}
+      />
+    </h1>
   );
+};
 
-
-export default Start
+export default Start;
--- frontend/kom_bat/src/components/mode/gamemode.js
+++ frontend/kom_bat/src/components/mode/gamemode.js
@@ -1,23 +1,21 @@
+import React from 'react';
 import H2 from "./H";
 import Mode_1 from "./mode_1";
 import Mode_2 from "./mode_2";
 import Mode_3 from "./mode_3";
+import BackBotton from "../BackBotton/BackBotton"; // เพิ่มปุ่มย้อนกลับเข้ามา
 import './gamemode.css'
+
 function gamemode() {
   return(
-    
-      <div className="mode" >
-        <H2/>
-        <Mode_1/>
-        <Mode_2/>    
-        <Mode_3/>
-        <h2>ออก</h2>
-
-      </div>
-   
+    <div className="mode">
+      <BackBotton /> {/* ปุ่มย้อนกลับใหม่ */}
+      <H2/>          {/* ของเก่า */}
+      <Mode_1/>      {/* ของเก่า */}
+      <Mode_2/>      {/* ของเก่า */}
+      <Mode_3/>      {/* ของเก่า */}
+    </div>
   );
-    
-  
 }
 
 export default gamemode;

093be5b7762bac7f4e68dbed531c76d8053fcf9c
pahnupong660610783 <pahnupong_kaeopramun@cmu.ac.th>
2025-03-23 17:50:21 +0700

    ตกแต่งกรอบหน้าตัวละคร

--- frontend/kom_bat/src/components/Character/Character.js
+++ frontend/kom_bat/src/components/Character/Character.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import BackBotton from '../BackBotton/BackBotton';
 import './Character.css';
 import { useNavigate } from 'react-router-dom';
+import bgImage from './รูป/BG1.webp';
+import ghost1 from './รูป/G1.png';
+import ghost2 from './รูป/G2.png';
+import ghost3 from './รูป/G3.png';
+import ghost4 from './รูป/G4.png';
+import ghost5 from './รูป/G5.png';
 
 const characters = [
-  { id: 1, name: "ผีดูดเลือด", img: "/images/ghost1.png" },
-  { id: 2, name: "ผีกุมาร", img: "/images/ghost2.png" },
-  { id: 3, name: "ผีเปรต", img: "/images/ghost3.png" },
-  { id: 4, name: "ผีนางรำ", img: "/images/ghost4.png" },
-  { id: 5, name: "ผีตายโหง", img: "/images/ghost5.png" },
+  { id: 1, name: "ผีเวตาล", img: ghost1 },
+  { id: 2, name: "ผีกุมาร", img: ghost2 },
+  { id: 3, name: "ผีเปรต", img: ghost3 },
+  { id: 4, name: "ผีนางรำ", img: ghost4},
+  { id: 5, name: "ผีตายโหง", img: ghost5 },
 ];
 
 export default function Character() {
@@ -29,7 +35,14 @@ export default function Character() {
   };
 
   return (
-    <div className="character-container">
+    <div
+      className="character-container"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}
+    >
       <h1>เลือกเหล่าวิญญาณร้ายที่คุณต้องการจะใช้</h1>
       <div className="character-list">
         {characters.map((char) => (

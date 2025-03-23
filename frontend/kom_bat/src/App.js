be7596c5eae7605079d02e6c69777bdd84d637b0
Phuwanat taratipprai <phuwanat_ta@cmu.ac.th>
2025-03-23 22:53:16 +0700

    ตกแต่งหน้าhome

--- frontend/kom_bat/src/App.js
+++ frontend/kom_bat/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home.js';
 import Gamemode from './components/mode/gamemode.js';
 import Character from './components/Character/Character.js';
-import Select from './components/Select/Select.js';
+import Select from './components/select/select.js';
 import Play from './components/Play/Play';
 
 function App() {

19fdac61ab4566a0b007f6ff6d79dabbf338fa0e
pairrz <dararat.buayim@gmail.com>
2025-03-24 20:35:55 +0700

    link-game mode

--- frontend/kom_bat/src/components/mode/mode_1.js
+++ frontend/kom_bat/src/components/mode/mode_1.js
@@ -5,17 +5,35 @@ import { useNavigate } from 'react-router-dom';
 const Mode_1 = () => {
     const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate('/waitingroom'); // ✅ กดแล้วไป waitingroom
+    const handleClick = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/create', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    playerOneName: "Player1",
+                    playerTwoName: "",
+                    gameMode: "DUEL_MODE"  // 👈 ส่งค่าโหมด
+                })
+            });
+
+            if (response.ok) {
+                navigate('/WaitingRoom'); // ✅ กดแล้วไป waitingroom
+            } else {
+                console.error("Failed to create game");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
     };
 
     return (
-        <img 
-            className="image1" 
-            src={B1} 
-            alt="Hover Image" 
-            onClick={handleClick} 
-            style={{ cursor: 'pointer' }} 
+        <img
+            className="image1"
+            src={B1}
+            alt="Hover Image"
+            onClick={handleClick}
+            style={{ cursor: 'pointer' }}
         />
     );
 };
